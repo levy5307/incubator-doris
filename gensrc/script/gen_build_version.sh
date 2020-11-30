def8cf199d9ab0c0408d397ed1f3fe1a4216d0db
Yingchun Lai <405403881@qq.com>
2020-11-30 14:24:46 +0800

    [MI][version] Bump version to 0.13.11-mdh1.0.0

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="0.13-branch-mdh"
+build_version="0.13.11-mdh1.0.0"
 
 unset LANG
 unset LC_CTYPE
