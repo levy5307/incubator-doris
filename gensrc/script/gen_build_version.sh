d709e842f20e003ce27f9febd85b3ebdcf8bc380
weizuo <weizuo@xiaomi.com>
2021-05-20 16:22:45 +0800

    Revert "Publish new version 0.13.11-mdh1.0.9 for doris"
    This reverts commit 7160693a70c2884a55357dec8eb0c593c2f3a69a.

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="0.13.11-mdh1.0.9"
+build_version="0.13.11-mdh1.0.8"
 
 unset LANG
 unset LC_CTYPE
