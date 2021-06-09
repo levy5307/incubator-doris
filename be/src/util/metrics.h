apache/doris

aggregate metric for any metric label

be/src/util/metrics.h
@@ -410,6 +410,8 @@ public:
     std::string to_prometheus(bool with_tablet_metrics = false) const;
     std::string to_json(bool with_tablet_metrics = false) const;
     std::string to_core_string() const;
+    std::string aggregate_metric(const std::string& entityType,
+                                 const std::string& aggregate_label) const;
 
 private:
     const std::string _name;

levy5307/incubator-doris

be/src/util/metrics.h
@@ -384,6 +384,8 @@ public:
     std::string to_prometheus(bool with_tablet_metrics = false) const;
     std::string to_json(bool with_tablet_metrics = false) const;
     std::string to_core_string() const;
+    std::string aggregate_metric(const std::string& entity_ype,
+                                 const std::string& aggregate_label) const;
 
 private:
     const std::string _name;