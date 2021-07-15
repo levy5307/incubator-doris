a9f9201bdbbd75a12541662d39e9b5c68fa02ce2
wangxixu <wangxixu@xiaomi.com>
2021-07-15 11:56:49 +0800

    publish new version 0.13.11-mdh1.1.6

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="0.13.11-mdh1.1.5"
+build_version="0.13.11-mdh1.1.6"
 
 unset LANG
 unset LC_CTYPE
