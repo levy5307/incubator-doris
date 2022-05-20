1f688c8dc4d53b9c6572cc0bf6426a64f94ad085
tianhui5 <827677355@qq.com>
2022-05-20 17:37:40 +0800

    [Version] Publish new version 0.13.11-mdh1.3.2

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="0.13.11-mdh1.3.1"
+build_version="0.13.11-mdh1.3.2"
 
 unset LANG
 unset LC_CTYPE
